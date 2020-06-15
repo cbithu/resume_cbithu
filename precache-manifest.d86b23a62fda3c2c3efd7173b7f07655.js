5ecff282778b2b76982cff90f1322588d2d44636
cbithu <45511692+cbithu@users.noreply.github.com>
2020-06-15 12:14:34 +0530

    Updates

---
+++ precache-manifest.d86b23a62fda3c2c3efd7173b7f07655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2049796b9e55c0df62b1e5c628222d",
+    "url": "/resume_cbithu/index.html"
+  },
+  {
+    "revision": "7f9b97b42249e47cc0a3",
+    "url": "/resume_cbithu/static/css/main.ad6e6a1b.chunk.css"
+  },
+  {
+    "revision": "2554204088645f7d6605",
+    "url": "/resume_cbithu/static/js/2.70ac3cbb.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/resume_cbithu/static/js/2.70ac3cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f9b97b42249e47cc0a3",
+    "url": "/resume_cbithu/static/js/main.f674c8c3.chunk.js"
+  },
+  {
+    "revision": "653a78a3398f59e5d5c0",
+    "url": "/resume_cbithu/static/js/runtime-main.819f08c9.js"
+  }
+]);
